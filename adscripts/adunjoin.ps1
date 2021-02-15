3ad4682bf54fdd4266a493776bbb75601f568728
Rakesh Singh <rksinghr@amazon.com>
2021-02-15 12:18:30 -0500

    initial commit

--- adscripts/adunjoin.ps1
+++ adscripts/adunjoin.ps1
@@ -2,25 +2,23 @@ param (
     [string]$ad_secret,
     [string]$ad_username,
     [string]$ad_domain_name,
-	[string]$instance_hostname
-
-	
+	[string]$instance_hostname	
 )
 $ErrorActionPreference = 'Stop'
 
 try{
-        # Parse the response and convert the Secret String JSON into an object
-        $username = $ad_domain_name.ToUpper() + "\" + $ad_username
+# Parse the response and convert the Secret String JSON into an object
+$username = $ad_domain_name.ToUpper() + "\" + $ad_username
 
-        $password = $ad_secret | ConvertTo-SecureString -AsPlainText -Force
+$password = $ad_secret | ConvertTo-SecureString -AsPlainText -Force
 
-        $credential = New-Object System.Management.Automation.PSCredential($username, $password)
-        echo "fetching machine name to be removed from the domain"
-        $MachineName = Get-ADComputer -filter "Name -eq '$instance_hostname'" -Credential $credential -Server $ad_domain_name -AuthType 0 | Select-Object Name | foreach { $_.Name }
-        
-		echo "Removing $MachineName computer from the AD domain"
-		Remove-ADComputer -Identity $MachineName -Credential $credential -Server $ad_domain_name -Confirm:$false
-		echo "Domain unjoin completed successfully for COMPUTER_NAME = $MachineName"
+$credential = New-Object System.Management.Automation.PSCredential($username, $password)
+echo "fetching machine name to be removed from the domain"
+$MachineName = Get-ADComputer -filter "Name -eq '$instance_hostname'" -Credential $credential -Server $ad_domain_name -AuthType 0 | Select-Object Name | foreach { $_.Name }
+
+echo "Removing $MachineName computer from the AD domain"
+Remove-ADComputer -Identity $MachineName -Credential $credential -Server $ad_domain_name -Confirm:$false
+echo "Domain unjoin completed successfully for COMPUTER_NAME = $MachineName"
         
 }
 catch [Exception]{
